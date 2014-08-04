bd55a29d33bbb4169423dcad3f46f795e5c7d765
David Joos <david.joos@gmail.com>
2014-08-04 21:20:09 +0100

    Ignore failures in package purging

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dev@escapestudios.com'
 license 'MIT'
 description 'Installs/Configures libfdk-aac'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 
 supports 'ubuntu'
 
